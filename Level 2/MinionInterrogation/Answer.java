670701992d934c2aad199059b579b81a8191f2e5
jeraguilon <jeraguilon@gmail.com>
2016-06-05 01:59:10 -0400

    Added new heuristic to enable O(N*log(N)) complexity. THe previuos solution was O(N\!), and would time out upon verification against Google's test cases.

--- Level 2/MinionInterrogation/Answer.java
+++ Level 2/MinionInterrogation/Answer.java
@@ -1,100 +1,74 @@
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
+import java.util.List;
 
 //O(N!) solution
 
-class MinionInfo {
-	int position;
-	int time;
-	double prb;
+class MinionInfo implements Comparable<MinionInfo> {
+    int position;
+    int time;
+    double prb;
 
-	public MinionInfo(int time, int numerator, int denominator, int position) {
-		this.position = position;
-		this.time = time;
-		this.prb = (double) numerator / denominator;
+    double points;
 
-	}
+    public MinionInfo(int time, int numerator, int denominator, int position) {
+        this.position = position;
+        this.time = time;
+        this.prb = (double) numerator / denominator;
+        this.points = time / ((double)numerator/denominator);
 
-	public String toString() {
-		return "Minion #" + position;
-	}
-}
+    }
 
-class MinionStats {
-	static MinionInfo[] bestOrder;
-	static double bestTime = Double.MAX_VALUE;
+    @Override
+    public int compareTo(MinionInfo other) {
+        if (this.points < other.points) return -1;
+        else if (this.points > other.points) return 1;
+        return 0;
+    }
+
+    public String toString() {
+        return "Minion #" + position;
+    }
 }
 
 public class Answer {   
 
 
     public static int[] answer(int[][] minions) { 
-    	MinionInfo[] minionList = new MinionInfo[minions.length];
+        MinionInfo[] minionList = new MinionInfo[minions.length];
         //initialize array to generate to store minions
         for (int i = 0; i < minions.length; i++) {
-        	int[] curr = minions[i];
-        	minionList[i] = new MinionInfo(curr[0], curr[1], curr[2], i);
+            int[] curr = minions[i];
+            minionList[i] = new MinionInfo(curr[0], curr[1], curr[2], i);
         }
 
-        MinionStats ms = new MinionStats();
 
-        permute(minionList, 0, ms);
+        List<MinionInfo> result = Arrays.asList(minionList);
 
-        MinionInfo[] bestOrder = ms.bestOrder;
+        Collections.sort(result);
 
-        int[] result = new int[bestOrder.length];
 
-        for (int i = 0; i < result.length; i++) {
-        	result[i] = bestOrder[i].position;
+        MinionInfo[] optimizedOrder = (MinionInfo[]) result.toArray();
+
+        int[] output = new int[minions.length];
+
+        for (int i = 0; i < output.length; i++) {
+            output[i] = optimizedOrder[i].position;
         }
 
-        return result;
+        return output;
+
 
     } 
-    
-    static void permute(MinionInfo[] arr, int k, MinionStats bestStats){
 
+    public static void main(String[] args) {
+        int[][] arg = new int[][] {{390, 185, 624}, {686, 351, 947},
+                                    {276, 1023, 1024}, {199, 148, 250}};
 
-        for(int i = k; i < arr.length; i++){
-            swap(arr, i, k);
-            permute(arr, k+1, bestStats);
-			swap(arr, k, i);
-        }
-        if (k == arr.length -1){
-        	double thisTime = generateExpected(arr);
+        System.out.println(Arrays.toString(answer(arg)));
 
-        	if (thisTime < MinionStats.bestTime) {
-        		MinionStats.bestTime = thisTime;
-        		MinionStats.bestOrder = Arrays.copyOf(arr, arr.length);
-
-        	}
-        	
-        }
     }
 
-    private static void swap(MinionInfo[] arr, int p1, int p2) {
-        MinionInfo temp = arr[p1];
-        arr[p1] = arr[p2];
-        arr[p2] = temp;
-    }
-    
-    private static double generateExpected(MinionInfo[] arr) {
-        return generateExpected(arr, 0);
-    }
-    
-    private static double generateExpected(MinionInfo[] arr, int position) {
-        MinionInfo curr = arr[position];
-        
-        if (position == arr.length - 1) {
-            //return total time, probablility of right answer is 100%
-            return arr[position].time;
-        }
-        
-        
-        return curr.time * curr.prb + (1.0 - curr.prb) 
-        		* (curr.time + generateExpected(arr, position + 1));
-    }
     
     
 }
