1260eca5f86e36a5dc5534596111fdadcf2f1510
jeraguilon <jeraguilon@gmail.com>
2016-06-26 19:12:28 -0400

    Can now pass 2/5 cases

--- src/Level 4/BreedingLikeRabbits/BreedingLikeRabbits.java
+++ src/Level 4/BreedingLikeRabbits/BreedingLikeRabbits.java
@@ -5,15 +5,22 @@ import java.util.*;
 import java.math.BigInteger;
 public class BreedingLikeRabbits {
     private static final BigInteger MAX_VALUE = (new BigInteger("10")).pow(25);
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final BigInteger TWO = new BigInteger("2");
+
     private static Map<BigInteger, BigInteger> map = new HashMap<>();
 
+    public static void main(String[] args) {
+        System.out.println(answer("100"));
+    }
+
     public static String answer(String strS) {
-        map.put(BigInteger.ZERO, BigInteger.ONE);
-        map.put(BigInteger.ONE, BigInteger.ONE);
-        map.put(new BigInteger("2"), new BigInteger("2"));
+        map.put(BigInteger.ZERO, ONE);
+        map.put(BigInteger.ONE, ONE);
+        map.put(new BigInteger("2"), TWO);
         BigInteger result = binarySearch(BigInteger.ZERO, MAX_VALUE, new BigInteger(strS));
         if (result != null) return result.toString();
-        else return "notfound";
+        else return "None";
     }
 
     public static BigInteger R(BigInteger key) {
@@ -21,10 +28,14 @@ public class BreedingLikeRabbits {
             return map.get(key);
         }
         BigInteger newValue;
-        if (key.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
-            newValue = R(key.divide(new BigInteger("2"))).add(R(key.divide(new BigInteger("2")).add(BigInteger.ONE))).add(key);
+        if (key.mod(TWO).equals(BigInteger.ZERO)) {
+            newValue = R(key.divide(TWO))
+                    .add(R(key.divide(TWO).add(ONE)))
+                    .add(key);
         } else {
-            newValue = R((key.subtract(BigInteger.ONE)).divide(new BigInteger("2")).subtract(BigInteger.ONE)).add(R((key.subtract(BigInteger.ONE)).divide(new BigInteger("2")))).add(BigInteger.ONE);
+            newValue = R((key.subtract(ONE)).divide(TWO).subtract(ONE))
+                    .add(R((key.subtract(ONE)).divide(TWO)))
+                    .add(BigInteger.ONE);
         }
 
         map.put(key, newValue);
@@ -32,23 +43,18 @@ public class BreedingLikeRabbits {
     }
 
     public static BigInteger binarySearch(BigInteger low, BigInteger high, BigInteger target) {
-        BigInteger midPoint = low.add(high.subtract(low).divide(new BigInteger("2")));
+        if (high.compareTo(low) < 0) return null;
 
-        if (high.compareTo(low) <= 0) {
-            return null;
-        }
+        BigInteger currentVal = low.add(high).divide(TWO);
 
-        BigInteger currentAnswer = R(midPoint);
-        if (currentAnswer.equals(target)) {
-            return midPoint;
-        }
-
-        if (currentAnswer.compareTo(target) < 0) {
-            return binarySearch(midPoint.add(BigInteger.ONE), high, target);
-        }
-
+        BigInteger output = R(currentVal);
+        if (target.equals(output)) return currentVal;
         else {
-            return binarySearch(low, midPoint.subtract(BigInteger.ONE), target);
+            if (target.compareTo(output) < 0) {
+                return binarySearch(low, currentVal.subtract(ONE), target);
+            } else {
+                return binarySearch(currentVal.add(ONE), high, target);
+            }
         }
     }
 
